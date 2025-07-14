860d64078c3d6c4f72e14491ce59342a904dad9b
naufal-h <naufal.humam-2021@fst.unair.ac.id>
2025-07-14 07:59:24 +0700

    feat: update API base URLs to use environment variables for configuration

--- src/lib/api-karyawan.ts
+++ src/lib/api-karyawan.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:3002/api";
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL_KARYAWAN || "";
 
 export async function apiFetch<T>(
   endpoint: string,
